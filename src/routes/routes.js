4c13ad0d86d71c5893ee5264648445cc15f120a4
akbar solikhov <pcfjkjhkk@gmail.com>
2024-10-16 15:47:08 +0500

    header component is ready

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
+import Header from '../components/header/Header'
 import DoneRequest from './done_request/DoneRequest'
 import Home from './home/Home'
 const routes = () => {
     return (
         <>
+        <Header/>
             <Route exact path='/'>
            <Home/>     
             </Route>
