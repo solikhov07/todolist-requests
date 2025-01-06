ca2c98877b60af5da2e66313789d073ef8675c64
solikhov07 <pcfjkjhkk@gmail.com>
2025-01-06 12:06:05 +0500

    carousel image changed, new main client comments added,  new font added

--- src/jwt/data-json.js
+++ src/jwt/data-json.js
@@ -2,7 +2,7 @@ import { AiOutlineFileDone } from "react-icons/ai";
 import { IoSettings } from "react-icons/io5";
 import { FaHome } from "react-icons/fa";
 import carousel_todolist_01 from '../assets/images/todo-list-01.jpg'
-import carousel_todolist_02 from '../assets/images/todo-list-02.webp'
+import carousel_todolist_02 from '../assets/images/todo-list-02.jpg'
 import carousel_todolist_03 from '../assets/images/todo-list-03.webp'
 const obj = {
     header_nav_ul_options: [
@@ -25,15 +25,15 @@ const obj = {
     main:{
         carousel_images: [
             {
-                heading: "Easy to create tasks",
+                heading: "This streamlined process empowers users to stay organized and efficient, helping them maintain focus on what truly matters without getting bogged down by complicated setups or technical barriers",
                 url:carousel_todolist_01
             },
             {
-                heading: "Understandable instructions",
+                heading: "The goal is to provide support that feels intuitive, making the learning process as smooth and efficient as possible while minimizing the need for constant clarification or assistance",
                 url: carousel_todolist_02
             },
             {
-                heading:"Convenient use",
+                heading:"By focusing on intuitive interfaces and streamlined functionality, the platform allows users to accomplish tasks quickly and efficiently",
                 url:carousel_todolist_03
             }
         ]
